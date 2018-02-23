1bd6070c85abbf569d406c8b3713f1b0c9a3483e
ianas <iana.simonenko1@gmail.com>
2018-02-23 15:21:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ianas/tipcalculation/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ianas.tipcalculation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
